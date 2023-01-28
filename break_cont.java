d0cd38a4491ce182964ffa6024c807e2f1a479ce
Angitha <angitha.g08@gmail.com>
2023-01-28 12:30:01 +0530

    add file into repo

---
+++ break_cont.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class break_cont{
+    public static void main(String[] args){
+        System.out.println("Enter number: ");
+        Scanner take_input= new Scanner(System.in);
+        num=take_input.nextInt()
+        
+    }
+}
---
+++ forLoop.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class forLoop {
+    public static void main(String[] args) {
+        //for(init; condition; increment/decrement)
+        // for(int a = 0; a<=5; a+=2){
+        //     System.out.println("Hello: "+a);
+        // Scanner take_input= new Scanner(System.in);
+        // System.out.println("Enter user input");
+        // int num_1 = take_input.nextInt();
+        // for (int i=1; i<=10; i+=1){
+        //     int mult_tbl=num_1*i;
+        //     System.out.println(+mult_tbl);
+        // Scanner myObj = new Scanner(System.in);
+        // System.out.println("Enter name, age, class ");
+        // String name=myObj.nextLine();
+        // int age = Integer.parseInt(myObj.nextLine());
+        // String Class = myObj.nextLine();
+        // System.out.println("Name: " +name+ " Age: "  +age+ " Class: " +Class);
+        // Scanner MyNewObj = new Scanner(System.in);
+        // System.out.println("Enter any number from 1 to 7");
+        // int day = MyNewObj.nextInt();
+        // switch (day){
+        //     case 1:
+        //     System.out.println("Monday");
+        //     break;
+        //     case 2:
+        //     System.out.println("Tuesday");
+        //     break;
+        //     case 3:
+        //     System.out.println("Wednesday");
+        //     break;
+        //     case 4:
+        //     System.out.println("Thursday");
+        //     break;
+        //     case 5:
+        //     System.out.println("Friday");
+        //     break;
+        //     case 6:
+        //     System.out.println("Saturday");
+        //     break;
+        //     case 7:
+        //     System.out.println("Sunday");
+        //     break;  
+
+           
+        }
+
+    }
+}
